d327903c0ba115e9177d04fcc71c464eba05ee23
mama719 <ciegalaxy2@gmail.com>
2025-04-25 10:45:45 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-from streamlit_option_menu import option_menu 
+# from streamlit_option_menu import option_menu 
 import home , account
 
 st.title(":blue[Proper record Tracker : ] :green[Todo's]")
