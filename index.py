1fb1d50f66d98016c2a71bb479e565643381b734
mama719 <ciegalaxy2@gmail.com>
2025-04-25 10:43:19 +0000

    Update index.py

--- index.py
+++ index.py
@@ -7,17 +7,18 @@ st.title(":blue[Proper record Tracker : ] :green[Todo's]")
 st.markdown("---")
 
 with st.sidebar :
-    app = option_menu(
-    menu_title = "Todo's" ,
-    menu_icon = ["table"] ,
-    options = ["Home" , "Account"] ,
-    icons = ["house-fill" , 'person-circle'] ,
-    default_index = 1 ,
-    styles = {
-        "icon" : {"font-size" : "23px"} ,
-        "nav-link" : {"font-size" : "20px"},
-        "nav-link-selected" : {"background-color" : "blue"}
-    }),
+    # app = option_menu(
+    # menu_title = "Todo's" ,
+    # menu_icon = ["table"] ,
+    # options = ["Home" , "Account"] ,
+    # icons = ["house-fill" , 'person-circle'] ,
+    # default_index = 1 ,
+    # styles = {
+    #     "icon" : {"font-size" : "23px"} ,
+    #     "nav-link" : {"font-size" : "20px"},
+    #     "nav-link-selected" : {"background-color" : "blue"}
+    # }),
+    app = st.selectbox("Home / Account" , ["Home" , "Account"])
 
 if "Home" in app :
     home.app()
